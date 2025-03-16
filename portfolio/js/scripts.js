df8c99d60c11ec8b7224d1aaacf6b3f5cf0dcc55
kystar <156368819+revokedkystar@users.noreply.github.com>
2025-03-16 00:11:20 +0000

    Delete portfolio directory

--- portfolio/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const video = document.getElementById("bg-video");
-
-    // Ensure video is muted for autoplay to work
-    video.muted = true; // Mute for autoplay on most browsers
-    video.play(); // Start playing the video once loaded
-});
